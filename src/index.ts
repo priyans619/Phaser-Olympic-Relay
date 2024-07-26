ad889a12f6f7cf4f7ae82afd8137b324fd459028
priyans619 <priyansh.2b@gmail.com>
2024-07-26 16:46:34 +0530

    Add game config with required types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello world!');
+import { Game, Types } from 'phaser';
+import { LoadingScene } from './scenes';
+
+const gameConfig: Types.Core.GameConfig = {
+	title: 'Phaser game tutorial',
+  type: Phaser.WEBGL,
+  parent: 'game',
+  backgroundColor: '#351f1b',
+  scale: {
+    mode: Phaser.Scale.ScaleModes.NONE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
+  render: {
+    antialiasGL: false,
+    pixelArt: true,
+  },
+  callbacks: {
+    postBoot: () => {
+      window.sizeChanged();
+    },
+  },
+  canvasStyle: `display: block; width: 100%; height: 100%;`,
+  autoFocus: true,
+  audio: {
+    disableWebAudio: false,
+  },
+  scene: [LoadingScene],
+};
