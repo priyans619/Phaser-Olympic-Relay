fda8228cddb31bf3674bea50d64708cf00d87b23
priyans619 <priyansh.2b@gmail.com>
2024-07-26 18:30:20 +0530

    loading first game character

--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -1,9 +1,21 @@
-import { Scene } from 'phaser';
+import { GameObjects, Scene } from 'phaser';
+
 export class LoadingScene extends Scene {
+  private king!: GameObjects.Sprite;
+
   constructor() {
     super('loading-scene');
   }
+
+  preload(): void {
+    this.load.baseURL = 'assets/';
+    // key: 'king'
+    // path from baseURL to file: 'sprites/king.png'
+    this.load.image('king', 'sprites/king.png');
+  }
+
   create(): void {
-    console.log('Loading scene was created');
+		this.king = this.add.sprite(100, 100, 'king');
+    this.king.setScale(0.5); // player resize to 50% of its original size
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const babelOptions = {
@@ -83,6 +85,14 @@ module.exports = {
       template: './index.html',
       inject: 'body',
       minify: false,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'assets',
+          to: 'assets',
+        },
+      ],
     }),
   ],
   devServer: {
